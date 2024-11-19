51cf48f57c9120e5a812f7473e8a43ed8b8dba7b
masa0108 <lewis.hamilton.2008champ@gmail.com>
2024-11-19 17:29:18 +0900

    firebase接続

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/src/app.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() {
   final widgetsBinding =
       WidgetsFlutterBinding.ensureInitialized(); //アプリ起動時のスプラッシュ画面表示
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, //firebase
+  );
   runApp(const MyApp());
 }
